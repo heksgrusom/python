779ee8ad520311db0c26677c1c2724b89fd3a1f7
heksgrusom <114354127+heksgrusom@users.noreply.github.com>
2022-10-12 20:59:31 +0300

    лаба3

--- лаба3 пайтон/завдання2_лаба3.py
+++ лаба3 пайтон/завдання2_лаба3.py
@@ -1,17 +1,19 @@
-def num_symbol(st):
-    sym = []
-    used = []
-    for elem in st:
-        if elem in sym:
-            sym.remove(elem)
-            used.append(elem)
-        elif elem not in used:
-            sym.append(elem)
-    return sym
+def fff(str1,str2):
+    for elem in str1:
+        k = 0
+        l = 0
+        l = str1.count(elem)
+        for elem1 in str2:
+            if elem == elem1:
+                k+=1
+        if (l == 1 and k == 1):
+            print(elem," ")
+        
  
  
 s1 = input("Enter s1 : ")
 s2 = input("Enter s2 : ")
-s1+=s2
-for elem in num_symbol(s1):
-    print(elem, end=' ')
+
+fff(s1,s2)
+
+
